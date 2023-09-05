8284d052fad2cdc4fb6aeb2391395f30735670b7
cgathua <114153640+cgathua@users.noreply.github.com>
2023-09-05 07:56:47 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,51 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+let containerDiv = document.querySelector('.containerDiv') as HTMLDivElement;
+let createGrid = document.getElementById('createGrid') as HTMLButtonElement;
+let gridWidth = document.getElementById('widthRange') as HTMLInputElement;
+let gridHeight = document.getElementById('heightRange') as HTMLInputElement;
+let colorInput = document.getElementById('colorInput') as HTMLInputElement;
+let eraseBtn = document.getElementById('eraseBtn') as HTMLButtonElement;
+let resetBtn = document.getElementById('resetBtn') as HTMLButtonElement;
+let widthValue = document.getElementById('widthValue') as HTMLSpanElement;
+let heightValue = document.getElementById('heightValue') as HTMLSpanElement;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+createGrid.addEventListener('click', () => {
+  containerDiv.textContent = "";
+  let count = 0;
+  for (let i = 0; i < +gridHeight.value; i++) { // gridHeight converted from string to number via '+'
+    count += 2;
+    let div = document.createElement('div');
+    div.classList.add('gridRow');
+    for (let j = 0; j < +gridWidth.value; j++) { // gridWidth converted from string to number via '+'
+      count += 2;
+      let col = document.createElement('div');
+      col.classList.add('gridCol');
+      col.getAttribute(`gridCol${count}`);
+      col.addEventListener('mouseover', () => {
+        col.style.backgroundColor = colorInput.value;
+      });
+      div.append(col);
+    }
+    containerDiv.append(div);
+  }
+});
+
+
+gridWidth.addEventListener('input', () => {
+  widthValue.textContent = gridWidth.value
+});
+
+gridHeight.addEventListener('input', () => {
+  heightValue.textContent = gridHeight.value;
+});
+
+eraseBtn.addEventListener('click', () => {
+  containerDiv.textContent = '';
+});
+
+
+resetBtn.addEventListener('click', () => {
+  gridHeight.value = '0';
+  gridWidth.value = '0';
+});
